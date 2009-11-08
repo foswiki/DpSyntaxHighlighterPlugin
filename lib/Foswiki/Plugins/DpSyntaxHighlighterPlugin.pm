88d6c32bf1e4152a11f401959f54dcf355592c11
AndrewJones <AndrewJones@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2009-11-08 18:21:23 +0000

    Item8185: upgrade the highlighter and apply patch and docs from JeffVStein
    git-svn-id: http://svn.foswiki.org/trunk/DpSyntaxHighlighterPlugin@5458 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/DpSyntaxHighlighterPlugin.pm
+++ lib/Foswiki/Plugins/DpSyntaxHighlighterPlugin.pm
@@ -20,7 +20,7 @@ use strict;
 use vars qw( $VERSION $RELEASE $NO_PREFS_IN_TOPIC $SHORTDESCRIPTION $pluginName $rootDir $doneHead );
 
 our $VERSION = '$Rev: 9813$';
-our $RELEASE = '1.3';
+our $RELEASE = '1.4';
 our $pluginName = 'DpSyntaxHighlighterPlugin';
 our $NO_PREFS_IN_TOPIC = 1;
 our $SHORTDESCRIPTION = 'Client side syntax highlighting using the [[http://code.google.com/p/syntaxhighlighter/][dp.SyntaxHighlighter]]';
@@ -48,19 +48,23 @@ sub commonTagsHandler {
 sub _handleTag {
 
     my %params = Foswiki::Func::extractParameters($1);
-    my $el = $params{el} || 'pre';
+    my $el = (lc$params{el} eq 'textarea' ? 'textarea' : 'pre');
     my $lang = lc$params{lang} || lc$params{_DEFAULT}; # language
     my $code = $2; # code to highlight
 
     # start
-    my $out = "<$el name='code' class='$lang";
+    my $out = "<$el name='code' class='brush: $lang\;";
     
     # attributes
-    $out .= ":nogutter" if lc$params{nogutter} eq 'on';
-    $out .= ":nocontrols" if lc$params{nocontrols} eq 'on';
-    $out .= ":collapse" if lc$params{collapse} eq 'on';
-    $out .= ":firstline[$params{firstline}]" if $params{firstline};
-    $out .= ":showcolumns" if lc$params{showcolumns} eq 'on';
+    $out .= " gutter: false;" if lc$params{nogutter} eq 'on';
+    $out .= " toolbar: false;" if lc$params{nocontrols} eq 'on';
+    $out .= " collapse: true;" if lc$params{collapse} eq 'on';
+    $out .= " first-line: $params{firstline};" if $params{firstline};
+    $out .= " wrap-lines: false;" if lc$params{nowrap} eq 'on';
+    $out .= " ruler: true;" if lc$params{ ruler} eq 'on';
+    $out .= " smart-tabs: false;" if lc$params{nosmarttabs} eq 'on';
+    $out .= " tab-size: $params{tabsize};" if $params{tabsize};
+    $out .= " highlight: [$params{highlight}];" if $params{highlight};
     $out .= "'";
 
     if ($el eq 'textarea') {
@@ -82,17 +86,23 @@ sub _handleTag {
     for ($lang){
 	/as3|actionscript3/ and $brush = "AS3", last;
 	/css/ and $brush = "Css", last;
+	/bash|shell/ and $brush = "Bash", last;
 	/c#|c-sharp|csharp/ and $brush = "CSharp", last;
-	/^c$|cpp|c\+\+/ and $brush = "Cpp", last;
-	/vb|vb\.net/ and $brush = "Vb", last;
+	/^c$|cpp/ and $brush = "Cpp", last;
+	/vb|vbnet/ and $brush = "Vb", last;
 	/delphi|pascal/ and $brush = "Delphi", last;
+	/diff|patch/ and $brush = "Diff", last;
+	/groovy/ and $brush = "Groovy", last;
 	/js|jscript|javascript/ and $brush = "JScript", last;
 	/^java$/ and $brush = "Java", last;
+	/jfx|javafx/ and $brush = "JavaFX", last;
 	/php/ and $brush = "Php", last;
-	/^pl$|perl/ and $brush = "Perl", last;
-	/plain|ascii/ and $brush = "Plain", last;
+	/^pl$|[Pp]erl/ and $brush = "Perl", last;
+	/plain|text/ and $brush = "Plain", last;
+	/ps|powershell/ and $brush = "PowerShell", last;
 	/py|python/ and $brush = "Python", last;
 	/ruby|ror|rails/ and $brush = "Ruby", last;
+	/scala/ and $brush = "Scala", last;
 	/sql/ and $brush = "Sql", last;
 	/xml|xhtml|xslt|html/ and $brush = "Xml", last;
     }
@@ -101,7 +111,7 @@ sub _handleTag {
     return "<span class='foswikiAlert'>$pluginName error: The language \"$lang\" is not supported.</span>"
         if $brush eq '';
 
-    $out .= "<script type=\"text/javascript\" src='$rootDir/Scripts/shBrush$brush.js'></script>";
+    $out .= "<script type=\"text/javascript\" src='$rootDir/scripts/shBrush$brush.js'></script>";
 		
     _doHead();
 	
@@ -115,27 +125,56 @@ sub _doHead {
     $doneHead = 1;
 
     # style sheet
-    my $style = "<style type='text/css' media='all'>\@import url($rootDir/Styles/SyntaxHighlighter.css);</style>";
+    my $style = "<link type='text/css' rel='stylesheet' href='$rootDir/styles/shCore.css' />";
+
+    # CSS theme to use for output
+    my $theme = Foswiki::Func::getPreferencesValue("\U$pluginName\_THEME");
+    $theme = 'Default' if $theme eq '';
+    $style .= "<link type='text/css' rel='stylesheet' href='$rootDir/styles/shTheme$theme.css' />";
+    
+    my $jsDefs = '';
+
+    # Hide about/print buttons
+    if (uc(Foswiki::Func::getPreferencesValue("\U$pluginName\_HIDE_ABOUT")) eq 'ON') {
+        $jsDefs .= "\n  delete SyntaxHighlighter.toolbar.items[\"about\"];";
+    }
+    if (uc(Foswiki::Func::getPreferencesValue("\U$pluginName\_HIDE_PRINT")) eq 'ON') {
+        $jsDefs .= "\n  delete SyntaxHighlighter.toolbar.items[\"printSource\"];";
+    }
+
+    # SyntaxHighlighter normally tries to guess the width of a space by
+    # measuring it through javascript. When measuring, it appends the item 
+    # to the document.body, so font-size may not match the Foswiki theme. 
+    #
+    # This allows an override in WebPreferences. The value is in pixels.
+    #    Set DPSYNTAXHIGHLIGHTERPLUGIN_SPACEWIDTH = 8
+    my $spaceWidth = Foswiki::Func::getPreferencesValue("\U$pluginName\_SPACEWIDTH");
+    if ($spaceWidth ne '') {
+        $jsDefs .= "\n  SyntaxHighlighter.vars.spaceWidth = $spaceWidth;";
+    }
             
     # core javascript file
-    my $core = "<script type=\"text/javascript\" src='$rootDir/Scripts/shCore.js'></script>";
+    my $core = "<script type=\"text/javascript\" src='$rootDir/scripts/shCore.js'></script>";
 
     my $script = <<"EOT";
 <script type="text/javascript">
-// use dp.SyntaxHighlighter namespace
-dp.SyntaxHighlighter.render = function(){
-  dp.SyntaxHighlighter.ClipboardSwf = '$rootDir/Scripts/clipboard.swf';
-  dp.SyntaxHighlighter.HighlightAll('code');
+// use SyntaxHighlighter namespace
+SyntaxHighlighter.render = function(){
+  SyntaxHighlighter.config.clipboardSwf = '$rootDir/scripts/clipboard.swf';
+  SyntaxHighlighter.config.tagName = 'pre';$jsDefs
+  SyntaxHighlighter.highlight();
+  SyntaxHighlighter.config.tagName = 'textarea';
+  SyntaxHighlighter.highlight();
 }
 if (typeof jQuery != 'undefined') {
   // jQuery
-  \$(document).ready(dp.SyntaxHighlighter.render);
+  \$(document).ready(SyntaxHighlighter.render);
 } else if (typeof YAHOO != "undefined") {
   // YUI
-  YAHOO.util.Event.onDOMReady(dp.SyntaxHighlighter.render);
+  YAHOO.util.Event.onDOMReady(SyntaxHighlighter.render);
 } else if (typeof foswiki != "undefined"){
   // foswiki
-  foswiki.Event.addLoadEvent(dp.SyntaxHighlighter.render);
+  foswiki.Event.addLoadEvent(SyntaxHighlighter.render);
 } else {
   alert("Can't add load event for $pluginName. Please contact your System Administrator.");
 }
